92bf9105d9483b6c81e03a3e2323aed6920bcd61
Alex Pearson <pearsonn.alex@gmail.com>
2015-08-19 17:44:56 +0000

    First commit master

---
+++ README.md
@@ -0,0 +1,11 @@
+# Intro-Class
+Here are the teaching and example materials for our Introductory Programming Class. In this repository, you will find:
+
++ A fully fleshed-out directory structure for the first website
++ Some neat code for a Javascript-based calculator
++ Not much else
+
+If you would like to use this material in your own classes, just give me a shout out on Twitter @NAlexPear. 
+If you're just learning the basics, you're more than welcome to use any of this material at any time.
+
+Thanks for taking a look, and good luck with this class!
---
+++ js/calculator.js
@@ -0,0 +1,43 @@
+var calc = function(){
+    var input1 = prompt("What is your first number?");
+    var input2 = prompt("What is your second number?");
+    var num1 = parseInt(input1, 0);
+    var num2 = parseInt(input2, 0);
+    
+    if(isNaN(num1) === true || isNaN(num2) === true){
+        alert("Please input two numbers. Click the button to try again!");
+    } else {
+        var action = prompt("What would you like to do with these numbers?");
+        
+        var add = function(num1, num2){
+            return num1+num2;
+        };
+        var sub = function(num1, num2){
+            return num1-num2;
+        };
+        var mult = function(num1, num2){
+            return num1*num2;
+        };
+        var div = function(num1, num2){
+            return num1/num2;
+        };
+        
+        if(action === 'add'){
+            alert(add(num1, num2));
+        } else if(action === 'subtract'){
+            alert(sub(num1, num2));
+        } else if(action === 'multiply'){
+            alert(mult(num1, num2));
+        } else if(action === 'divide'){
+            alert(div(num1, num2));
+        } else {
+            alert("Please input a valid action! Try 'add', 'subtract', 'multiply', or 'divide'.");
+        }
+    }
+};
+
+$(document).ready(function(){
+    $('#button').click(function(){
+        calc();
+    });
+});
