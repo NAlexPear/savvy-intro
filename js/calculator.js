76c4194ab6f044f4b4e8a0d20233a11dccb62823
Alex Pearson <apearson@five-startech.com>
2016-04-26 16:16:19 -0500

    Refactor calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,20 @@
-var calc = function(){
+var add = function(num1, num2){
+    return num1 + num2;
+};
+var sub = function(num1, num2){
+    return num1 - num2;
+};
+var mult = function(num1, num2){
+    return num1 * num2;
+};
+var div = function(num1, num2){
+    return num1 / num2;
+};
+var mod = function(num1, num2) {
+    return num1 % num2;
+};
+
+var parseNumbers = function(){
     var input1 = prompt("What is your first number?");
     var input2 = prompt("What is your second number?");
     var num1 = parseInt(input1, 0);
@@ -6,44 +22,43 @@ var calc = function(){
     
     if(isNaN(num1) === true || isNaN(num2) === true){
         alert("Please input two numbers. Click the button to try again!");
-    } else {
-        var action = prompt("What would you like to do with these numbers?");
-        
-        var add = function(num1, num2){
-            return num1+num2;
-        };
-        var sub = function(num1, num2){
-            return num1-num2;
-        };
-        var mult = function(num1, num2){
-            return num1*num2;
-        };
-        var div = function(num1, num2){
-            return num1/num2;
-        };
-        var mod = function(num1, num2) {
-            return num1%num2;
-        };
-        
-        if(action === 'add'){
-            alert(add(num1, num2));
-        } else if(action === 'subtract'){
-            alert(sub(num1, num2));
-        } else if(action === 'multiply'){
-            alert(mult(num1, num2));
-        } else if(action === 'divide'){
-            alert(div(num1, num2));
-        } else if(action === 'modulo'){
-            alert(mod(num1, num2));
-        }
-        else {
-            alert("Please input a valid action! Try 'add', 'subtract', 'multiply', or 'divide'.");
-        }
+    } else { 
+        return [ input1, input2 ];
     }
 };
 
-$(document).ready(function(){
-    $('#button').click(function(){
-        calc();
-    });
+var parseOperation = function( action, num1, num2 ){
+    if(action === 'add'){
+        alert(add(num1, num2));
+    } else if(action === 'subtract'){
+        alert(sub(num1, num2));
+    } else if(action === 'multiply'){
+        alert(mult(num1, num2));
+    } else if(action === 'divide'){
+        alert(div(num1, num2));
+    } else if(action === 'modulo'){
+        alert(mod(num1, num2));
+    } else {
+        alert("Please input a valid action! Try 'add', 'subtract', 'multiply', or 'divide'.");
+    }
+};
+
+var calc = function(){
+    var input = parseNumbers();
+    var action = "";
+    var num1;
+    var num2;
+    
+    if( input ){
+        num1 = input[0];
+        num2 = input[1];
+        action = prompt("What would you like to do with these numbers?");
+        
+        parseOperation( action, num1, num2 );
+    }
+};
+
+var button = document.querySelector("#button");
+button.addEventListener("click", function(){
+    calc();
 });
